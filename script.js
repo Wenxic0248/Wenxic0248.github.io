8e6981de0fafb801249012594e78e6807251373f
Wenxic0248 <wenxic5@nycstudents.net>
2025-09-30 09:19:47 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let ownerName = "Your Name"; // update the part BETWEEN the "quotes"
-let userName = "YourUsername"; // same here
+let ownerName = "Wen Xi"; // update the part BETWEEN the "quotes"
+let userName = "Wenxic0248"; // same here
 
 document.querySelectorAll(".owner-name").forEach((e) => {
     e.innerHTML = ownerName;
